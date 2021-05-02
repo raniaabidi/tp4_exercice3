65a0b61e7a87e140e780db69631742ef6f4644d9
Rania Abidi <raniaabidi987@gmail.com>
2021-05-02 15:07:02 +0100

    tp4_exercice2

---
+++ src/authentification/session.java
@@ -0,0 +1,52 @@
+package authentification;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class session
+ */
+@WebServlet("/session")
+public class session extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public session() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String a = request.getParameter("t1");
+		String b = request.getParameter("t2");
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("t1",a);
+		session.setAttribute("t2",b);
+		
+		RequestDispatcher dispatcher =request.getRequestDispatcher("resultata.jsp");
+		dispatcher.forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
